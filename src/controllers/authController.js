c566a8d7fb24b4128211c9e2fba417ce91a29588
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-05-15 22:52:18 -0300

    add documentação

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -3,22 +3,22 @@ const User = require('../models/User');
 
 const router = express.Router();
 
-router.post('/register', async (req, res) => {
-    const { email } = req.body;
+router.post('/register', async (req, res) => { //rota post de registro
+    const { email } = req.body;                 // destruturando  do corpo da requisição o email
 
     try {
         if (await User.findOne({ email })) 
-        return res.status(400).send( {error: 'User already exists'});
+        return res.status(400).send( {error: 'User already exists'}); // se o email já existir no BD retorna mensagem usuario já existe
 
-        const user = await User.create(req.body);
+        const user = await User.create(req.body);  // cria usuario com dados  do orpo da requisição
         
-        user.password = undefined;
+        user.password = undefined;       // apaga o password criado para nao ficar visivel
 
-        return res.send({ user });
+        return res.send({ user });    // retorna usuario criado.
 
     }catch (err) {
-        return res.status(400).send({error: 'Registration failed'});
+        return res.status(400).send({error: 'Registration failed'});  //retorna erro de falha de criação de novo usuário
     }
 });
 
-module.exports = app => app.use('/auth', router);
+module.exports = app => app.use('/auth', router);  
