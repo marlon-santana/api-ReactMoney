d09af4455ce8f4276f1e4d427eb066ec4af8a679
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-05-18 22:58:54 -0300

    parametros de requisiçoes

--- index.js
+++ index.js
@@ -6,18 +6,25 @@ app.listen(3000);
 
 app.use(express.json());
 
-let author = 'marlon de oliveira';
+// let author = 'marlon de oliveira';
 
-app.route('/').get((req, res) =>  res.send(author));
+// app.route('/').get((req, res) =>  res.send(author));
 
- app.route('/').post((req, res) => res.send(req.body))//recebe o content da requisição e devolve essa informação na resposta.
+//  app.route('/').post((req, res) => res.send(req.body))//recebe o content da requisição e devolve essa informação na resposta.
  
- app.route('/').put((req, res) => {
-     author = req.body;
-     res.send(author)
-    })
+//  app.route('/').put((req, res) => {
+//      author = req.body;
+//      res.send(author)
+//     })
 
     /********Requisições com  parametros************/
-   app.route('/').get((req, res) => res.send(req.query.name));
-   app.route('/').post((req, res) => req.send(req.body));
-   app.route('/:parametro').get((req, res) => res.send(req.params.parametro))
+//    app.route('/').get((req, res) => res.send(req.query.name));
+//    app.route('/').post((req, res) => req.send(req.body));
+//    app.route('/:parametro').get((req, res) => res.send(req.params.parametro))
+app.use(express.json());
+
+app.route('/').post((req, res) => {
+    const {nome, idade } = req.body
+
+    res.send(`o nome é ${nome} e a idade é ${idade}`)
+});
