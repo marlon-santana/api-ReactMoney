82a6e58a641ccee0d272fe13a0f202bc077a966f
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-05-20 10:06:06 -0300

    enviando obj json

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000);
-
 app.use(express.json());
 
-// let author = 'marlon de oliveira';
+ let author = {
+     "nome":"marlon de oliveira",
+     "age":"36",
+     "city":"nova iguaçu",
+ }
 
-// app.route('/').get((req, res) =>  res.send(author));
+ app.route('/').get((req, res) =>  res.send(author));
 
 //  app.route('/').post((req, res) => res.send(req.body))//recebe o content da requisição e devolve essa informação na resposta.
  
@@ -21,9 +23,12 @@ app.use(express.json());
 //    app.route('/').get((req, res) => res.send(req.query.name));
 //    app.route('/').post((req, res) => req.send(req.body));
 //    app.route('/:parametro').get((req, res) => res.send(req.params.parametro))
+
 app.use(express.json());
 
 app.route('/').post((req, res) => {
     const {nome, idade } = req.body
     res.send(`o nome é ${nome} e a idade é ${idade}`) // api recebe nome e idade no body e repassa  de novo como resposta a frase
 });                                                     //com as variaves.
+
+app.listen(3000);
