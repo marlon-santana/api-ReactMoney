f3c3507859fe2b429339816a36c49931b3268591
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-05-23 01:13:01 -0300

    lincando mongodb com api

---
+++ src/database/index.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(
+    'mongodb+srv://marlon:marlonsantana4@cluster0.co3lc.mongodb.net/?retryWrites=true&w=majority',
+    // { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+    () => {
+      console.log("banco de dados conectado");
+    }
+  );
+  
+  module.exports = mongoose;
