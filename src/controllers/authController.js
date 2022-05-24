d98d3c2594b726fb663500e2e28cd5210d7ff624
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-05-24 00:44:21 -0300

    aiutenticaçãpo e geração de token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const authConfig = require('../config/auth');
 
 const User = require('../models/User');
 
@@ -23,4 +26,24 @@ router.post('/register', async (req, res) => {
         });
     }
 })
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body;
+
+    const user =  await User.findOne({ email}).select('+password');
+
+    if(!user) 
+    return res.status(400).send({ error: 'User not found '});
+
+    if(!await bcrypt.compare(password, user.password))
+    return res.status(400).send({ error: 'Invalid password'});
+
+    user.password = undefined;
+
+    const token = jwt.sign({id: user.id },authConfig.secret, {
+        expiresIn: 86400, // expiração em 24horas.
+    } )
+
+    res.send({ user, token });
+});
 module.exports = app => app.use('/auth', router);
