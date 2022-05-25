e42e91c61b43b5ea800a1183ecbad8fa34fbadcc
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-05-24 23:55:27 -0300

    add validação de token

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -7,6 +7,12 @@ const User = require('../models/User');
 
 const router = express.Router();
 
+ function generateToken(params = {}) {
+    return jwt.sign(params, authConfig.secret, {
+        expiresIn: 86400 // 24horas.
+    })
+ }
+
 
 router.post('/register', async (req, res) => {
     const { email } = req.body;
@@ -19,7 +25,10 @@ router.post('/register', async (req, res) => {
 
         user.password = undefined;
 
-        return res.send({user});
+        return res.send({user,
+            token: generateToken({ id: user.id }),
+        });
+
     }catch (err) {
         return res.status(400).send({
             error: 'Registration failed'
@@ -40,10 +49,10 @@ router.post('/authenticate', async (req, res) => {
 
     user.password = undefined;
 
-    const token = jwt.sign({id: user.id },authConfig.secret, {
-        expiresIn: 86400, // expiração em 24horas.
-    } )
 
-    res.send({ user, token });
+    res.send({ user,
+         token: generateToken({ id: user.id}),
+
+         });
 });
 module.exports = app => app.use('/auth', router);
